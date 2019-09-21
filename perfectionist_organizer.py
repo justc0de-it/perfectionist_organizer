abe6c7029b142b88796698548a6adee027bdc70b
Arkadiy Illarionov <qarkai@gmail.com>
2019-09-21 23:59:51 +0300

    Use os.path

--- perfectionist_organizer.py
+++ perfectionist_organizer.py
@@ -1,6 +1,5 @@
 #Импорт необходимых библиотек
 import os
-import getpass
 import platform
 
 # Создание словарей с расширениями файлов и папкой, куда их нужно перекинуть
@@ -23,7 +22,7 @@ def get_user_downloads_path(type_os):
 def move_files(downloads_files, file_format, downloads_path, format_folder):
     for name_file in downloads_files:
         if name_file.endswith(file_format):
-            os.rename(downloads_path + name_file, format_folder + name_file)
+            os.rename(os.path.join(downloads_path, name_file), os.path.join(format_folder, name_file))
 
 
 def main():
@@ -32,15 +31,10 @@ def main():
     if type_os not in ("Linux", "Windows"):
         return
     user_downloads_path = get_user_downloads_path(type_os)
-    #Определяем имя пользователя в системе
-    usermane = getpass.getuser()
     # Путь вида /домашняяпапка/имяпользователя
-    if type_os == "Linux":
-        default_path_u = "/home/" + usermane + "/"
-    else:
-        default_path_u = r"C:/Users/" + usermane + r"/"
+    default_path_u = os.path.expanduser('~')
     #Путь до папки с загрузками
-    default_path_d = default_path_u + user_downloads_path + "/"
+    default_path_d = os.path.join(default_path_u, user_downloads_path)
     #Задаём путь к папке с загрузками для конкретного пользователя
     downloads_path = os.listdir(default_path_d)
 
@@ -50,14 +44,14 @@ def main():
         folders = {"Videos/": video_formats, "Music/": music_formats, "Pictures/": pic_formats, "Documents/": doc_formats}
     for format_folder, file_formats in folders:
         for file_format in file_formats:
-            move_files(downloads_path, file_format, default_path_d, default_path_u + format_folder)
+            move_files(downloads_path, file_format, default_path_d, os.path.join(default_path_u, format_folder))
 
     #Запрос на удаление оставшихся файлов в директории загрузок
     delete_user_confirm = input('Удалить из папки загрузок оставшиеся файлы? Напишите да или нет (по-умолчанию: нет) ' or 'нет')
     if delete_user_confirm == 'да':
         files_to_remove = os.listdir(default_path_d)
         for remove_files in files_to_remove:
-            os.remove(default_path_d + "/" + remove_files)
+            os.remove(os.path.join(default_path_d, remove_files))
     else:
         print('Программа завершила работу. Все файлы размещены.')
 
