afa5227584740cd1552d174737b93deff5cf1a26
Arkadiy Illarionov <qarkai@gmail.com>
2019-09-22 00:55:12 +0300

    Fix format, comments

--- perfectionist_organizer.py
+++ perfectionist_organizer.py
@@ -1,9 +1,8 @@
-#Импорт необходимых библиотек
-import os
-import platform
+# Импорт необходимых библиотек
 from pathlib import Path
+import platform
 
-# Создание словарей с расширениями файлов и папкой, куда их нужно перекинуть
+# Создание списков с расширениями файлов
 video_formats = [".3gp", ".avi", ".flv", ".m4v", ".mkv", ".mov", ".mp4", ".wmv", ".webm"]
 music_formats = [".mp3", ".aac", ".flac", ".mpc", ".wma", ".wav"]
 pic_formats = [".raw", ".jpg", ".tiff", ".psd", ".bmp", ".gif", ".png", ".jp2", ".jpeg"]
@@ -11,7 +10,7 @@ doc_formats = [".doc", ".docx", ".txt", ".rtf", ".pdf", ".fb2", ".djvu", ".xls",
                ".xlsx", ".ppt", ".pptx", ".mdb", ".accdb", ".rar", ".zip", ".7z"]
 
 
-# Справшиваем у юзверя, как называется папка с загрузками (по-умолчанию: Загрузки)
+# Спрашиваем у юзверя, как называется папка с загрузками (по-умолчанию: Загрузки)
 def get_user_downloads_path(type_os):
     if type_os == "Linux":
         return input("Как у вас называется папка с загрузками? (по-умолчанию: Загрузки) ") or "Загрузки"
@@ -20,16 +19,17 @@ def get_user_downloads_path(type_os):
 
 
 def main():
-    #Проверка операционной системы, которой пользуется юзер
+    # Проверка операционной системы, которой пользуется юзер
     type_os = platform.system()
     if type_os not in ("Linux", "Windows"):
         return
     user_downloads_path = get_user_downloads_path(type_os)
     # Путь вида /домашняяпапка/имяпользователя
     default_path_u = Path.home()
-    #Путь до папки с загрузками
+    # Путь до папки с загрузками
     default_path_d = default_path_u / user_downloads_path
 
+    # Создание словарей с расширениями файлов и папкой, куда их нужно перекинуть
     if type_os == "Linux":
         folders = {"Видео": video_formats, "Музыка": music_formats, "Изображения": pic_formats, "Документы": doc_formats}
     else:
@@ -41,7 +41,7 @@ def main():
             if name_file.suffix in file_formats:
                 name_file.rename(default_path_u / format_folder / name_file.name)
 
-    #Запрос на удаление оставшихся файлов в директории загрузок
+    # Запрос на удаление оставшихся файлов в директории загрузок
     delete_user_confirm = input('Удалить из папки загрузок оставшиеся файлы? Напишите да или нет (по-умолчанию: нет) ' or 'нет')
     if delete_user_confirm == 'да':
         for remove_files in default_path_d.iterdir():
